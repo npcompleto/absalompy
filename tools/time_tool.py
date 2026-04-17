e324aa10a05b84f170042977db8d31f304b3dd01
npcompleto <npcompleto@gmail.com>
2026-04-17 18:22:44 +0200

    a

--- tools/time_tool.py
+++ tools/time_tool.py
@@ -18,4 +18,4 @@ def get_current_time():
     print("Sto controllando l'ora...")
     current_time = datetime.now().strftime("%H:%M")
     print(f"L'ora attuale è: {current_time}")
-    return f"L'ora attuale è: {current_time}"
+    return current_time
