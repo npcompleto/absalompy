2d281edbafd5fc484d972dbf0647af3fa73314b1
npcompleto <npcompleto@gmail.com>
2026-04-27 21:09:09 +0200

    aaa

--- workers/ingest_worker.py
+++ workers/ingest_worker.py
@@ -3,6 +3,7 @@ import time
 from tts_manager import TTSManager
 from agent import Agent
 from face_client import FaceClient
+import logging
 
 class IngestWorker:
     def __init__(self, face: FaceClient):
@@ -11,7 +12,13 @@ class IngestWorker:
     def run(self):
         while True:
             # Check if there are at least a file in the raw folder
-            raw_files = os.listdir("persona/knowledge/raw_documents")
+            try:
+                raw_files = os.listdir("persona/knowledge/raw_documents")
+            except Exception as e:
+                logging.error(f"Errore durante la lettura della cartella raw_documents, la creo: {e}")
+                #create folder
+                os.makedirs("persona/knowledge/raw_documents", exist_ok=True)
+                continue
             if len(raw_files) > 0:
                 TTSManager().speak("Ho dei documenti da archiviare.")
                 response = Agent().ask("Aggiorna knowledge raw_documents")
